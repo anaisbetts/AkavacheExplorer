5f072d1d65d36ec7e624a3ed80121fd92c5a3a52
Paul Betts <paul@paulbetts.org>
2012-05-07 16:10:40 -0700

    ViewModel code to see some keys, :metal:

--- AkavacheExplorer/ViewModels/CacheViewModel.cs
+++ AkavacheExplorer/ViewModels/CacheViewModel.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Linq;
 using ReactiveUI;
 using ReactiveUI.Routing;
 
@@ -6,16 +8,39 @@ namespace AkavacheExplorer.ViewModels
 {
     public interface ICacheViewModel : IRoutableViewModel
     {
+        ReactiveCollection<string> Keys { get; }
+        string SelectedKey { get; set; }
     }
 
     public class CacheViewModel : ReactiveObject, ICacheViewModel
     {
-        public string UrlPathSegment { get; protected set; }
+        public ReactiveCollection<string> Keys { get; protected set; }
+
+        string _SelectedKey;
+        public string SelectedKey {
+            get { return _SelectedKey; }
+            set { this.RaiseAndSetIfChanged(x => x.SelectedKey, value); }
+        }
+
+        ObservableAsPropertyHelper<string> _UrlPathSegment;
+        public string UrlPathSegment {
+            get { return _UrlPathSegment.Value; }
+        }
+
         public IScreen HostScreen { get; protected set; }
 
         public CacheViewModel(IScreen hostScreen, IAppState appState)
         {
             HostScreen = hostScreen;
+
+            appState.WhenAny(x => x.CachePath, x => "cache_" + (new DirectoryInfo(x.Value)).Name)
+                .ToProperty(this, x => x.UrlPathSegment);
+
+            Keys = new ReactiveCollection<string>();
+            appState.WhenAny(x => x.CurrentCache, x => x.Value).Subscribe(cache => {
+                Keys.Clear();
+                cache.GetAllKeys().ForEach(x => Keys.Add(x));
+            });
         }
     }
 }
