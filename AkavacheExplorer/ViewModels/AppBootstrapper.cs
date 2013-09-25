556d1e386ca18967af5840cc4f2d7d0de890de97
Paul Betts <paul@paulbetts.org>
2013-09-25 11:02:05 -0700

    lolninject

--- AkavacheExplorer/ViewModels/AppBootstrapper.cs
+++ AkavacheExplorer/ViewModels/AppBootstrapper.cs
@@ -1,6 +1,5 @@
 using Akavache;
 using AkavacheExplorer.Views;
-using Ninject;
 using ReactiveUI;
 
 namespace AkavacheExplorer.ViewModels
@@ -27,39 +26,28 @@ namespace AkavacheExplorer.ViewModels
             set { this.RaiseAndSetIfChanged(ref _CachePath, value); }
         }
 
-        public AppBootstrapper(IKernel kernel = null, IRoutingState router = null)
+        public AppBootstrapper(IDependencyResolver kernel = null, IRoutingState router = null)
         {
-            kernel = kernel ?? createStandardKernel();
+            RxApp.DependencyResolver = kernel ?? createStandardKernel();
             Router = router ?? new RoutingState();
-
-            RxApp.DependencyResolver.ConfigureServiceLocator(
-                (t, s) => kernel.Get(t, s),
-                (t, s) => kernel.GetAll(t, s),
-                (c, t, s) => kernel.Bind(t).To(c));
 
             // Our first screen is "Open cache"
             Router.Navigate.Execute(RxApp.DependencyResolver.GetService<IOpenCacheViewModel>());
         }
 
-        IKernel createStandardKernel()
+        IDependencyResolver createStandardKernel()
         {
-            var ret = new StandardKernel();
+            var r = new ModernDependencyResolver();
 
-            ret.Bind<IScreen>().ToConstant(this);
-            ret.Bind<IAppState>().ToConstant(this);
-            ret.Bind<IOpenCacheViewModel>().To<OpenCacheViewModel>();
-            ret.Bind<IViewFor<OpenCacheViewModel>>().To<OpenCacheView>();
-            ret.Bind<ICacheViewModel>().To<CacheViewModel>();
-            ret.Bind<IViewFor<CacheViewModel>>().To<CacheView>();
+            r.RegisterConstant(this, typeof(IScreen));
 
-            ret.Bind<ICacheValueViewModel>().To<TextValueViewModel>().Named("Text");
-            ret.Bind<ICacheValueViewModel>().To<JsonValueViewModel>().Named("Json");
-            ret.Bind<ICacheValueViewModel>().To<ImageValueViewModel>().Named("Image");
-            ret.Bind<IViewFor<TextValueViewModel>>().To<TextValueView>();
-            ret.Bind<IViewFor<JsonValueViewModel>>().To<JsonValueView>();
-            ret.Bind<IViewFor<ImageValueViewModel>>().To<ImageValueView>();
+            r.Register(() => new OpenCacheView(), typeof(IViewFor<OpenCacheViewModel>));
+            r.Register(() => new CacheView(), typeof(IViewFor<CacheViewModel>));
+            r.Register(() => new TextValueView(), typeof(IViewFor<TextValueViewModel>));
+            r.Register(() => new JsonValueView(), typeof(IViewFor<JsonValueViewModel>));
+            r.Register(() => new ImageValueView(), typeof(IViewFor<ImageValueViewModel>));
 
-            return ret;
+            return r;
         }
     }
 }
